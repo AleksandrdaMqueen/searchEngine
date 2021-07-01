fe87d01eed0be42486d3a3e8c8c2ac62a591ee70
Александр Банкевич <aleksandr@bankevich.com>
2021-07-01 20:15:36 +0000

    Update Main.java

--- 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
+++ 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
@@ -3,5 +3,13 @@ package ru.skillbox;
 public class Main {
 
     public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(8, 9, 9);
+        CargoInfo cargo = new CargoInfo(dimensions, 9, "Pushkinskaya", true, 0, true);
+        cargo.setDeliveryAddress("Tallinskaya");
+        System.out.println(dimensions);
+        dimensions.getVolume();
+
+        System.out.println(cargo);
+        System.out.println(dimensions);
     }
 }
