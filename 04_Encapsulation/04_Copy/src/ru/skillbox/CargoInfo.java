0e8c6d2bce18c69cfaff34632652d5922276926e
Александр Банкевич <aleksandr@bankevich.com>
2021-07-01 20:14:45 +0000

    Update CargoInfo.java

--- 04_Encapsulation/04_Copy/src/ru/skillbox/CargoInfo.java
+++ 04_Encapsulation/04_Copy/src/ru/skillbox/CargoInfo.java
@@ -1,17 +1,17 @@
 public class CargoInfo {
-    private final int dimensions;
+    private final Dimensions dimensions;
     private final int weight;
-    private final String deliveryAdress;
+    private final String deliveryAddress;
     private final boolean canBeTurnedOver;
-    private final int registratioNumber;
+    private final int registrationNumber;
     private final boolean whetherTheCargoIsFragile;
 
-    public CargoInfo(int dimensions, int weight, String deliveryAddress, boolean canBeTurnedOver, int registratioNumber, boolean whetherTheCargoIsFragile) {
+    public CargoInfo(Dimensions dimensions, int weight, String deliveryAddress, boolean canBeTurnedOver, int registrationNumber, boolean whetherTheCargoIsFragile) {
         this.dimensions = dimensions;
         this.weight = weight;
-        this.deliveryAdress = deliveryAddress;
+        this.deliveryAddress = deliveryAddress;
         this.canBeTurnedOver = canBeTurnedOver;
-        this.registratioNumber = registratioNumber;
+        this.registrationNumber = registrationNumber;
         this.whetherTheCargoIsFragile = whetherTheCargoIsFragile;
     }
 
@@ -20,32 +20,42 @@ public class CargoInfo {
     }
 
     public String getDeliveryAddress() {
-        return deliveryAdress;
+        return deliveryAddress;
     }
 
     public boolean isCanBeTurnedOver() {
         return canBeTurnedOver;
     }
 
-    public int getRegistratioNumber() {
-        return registratioNumber;
+    public int getRegistrationNumber() {
+        return registrationNumber;
     }
 
     public boolean isWhetherTheCargoIsFragile() {
         return whetherTheCargoIsFragile;
     }
 
-    public int getDimensions() {
+    public Dimensions getDimensions() {
         return dimensions;
     }
 
-    public CargoInfo setDeliveryAdress(String deliveryAdress) {
-        return new CargoInfo(dimensions, weight, deliveryAdress, canBeTurnedOver, registratioNumber, whetherTheCargoIsFragile);
+    public CargoInfo setDeliveryAddress(String deliveryAddress) {
+        return new CargoInfo(dimensions, weight, deliveryAddress, canBeTurnedOver, registrationNumber, whetherTheCargoIsFragile);
     }
 
-    public CargoInfo setDimensions(int dimensions) {
-        return new CargoInfo(dimensions, weight, deliveryAdress, canBeTurnedOver, registratioNumber, whetherTheCargoIsFragile);
+    public CargoInfo setDimensions(Dimensions dimensions) {
+        return new CargoInfo(dimensions, weight, deliveryAddress, canBeTurnedOver, registrationNumber, whetherTheCargoIsFragile);
     }
 
-
+    @Override
+    public String toString() {
+        return "CargoInfo" +
+                "dimensions = " + dimensions + '\n' +
+                " weight = " + weight + '\n' +
+                " deliveryAddress = '" + deliveryAddress + '\'' + '\n' +
+                " canBeTurnedOver = " + canBeTurnedOver + '\n' +
+                " registrationNumber = " + registrationNumber + '\n' +
+                " whetherTheCargoIsFragile = " + whetherTheCargoIsFragile;
+    }
 }
+
