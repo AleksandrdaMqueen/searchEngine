0cd7285313136d0e6a4861c5f340973e53c0d6fd
Александр Банкевич <aleksandr@bankevich.com>
2021-07-05 00:01:17 +0000

    Update Main.java

--- 05_StaticFinal/02_enum/src/ru/skillbox/Main.java
+++ 05_StaticFinal/02_enum/src/ru/skillbox/Main.java
@@ -1,7 +1,9 @@
-package ru.skillbox;
-
 public class Main {
-
     public static void main(String[] args) {
+
+        ArithmeticCalculator.calculate(1,2, Operation.ADD);
+        ArithmeticCalculator.calculate(1,2,Operation.SUBTRACT);
+
+
     }
 }
