c7db98c04114234366243dcf87fdd1bc1604a83c
Александр Банкевич <aleksandr@bankevich.com>
2021-07-06 21:27:37 +0000

    Upload New File

---
+++ 05_StaticFinal/03_computer/src/ru/skillbox/RAM.java
@@ -0,0 +1,12 @@
+public class RAM {
+    private final RAM type;
+    private final RAM volume;
+    private final RAM weight;
+    //===================================================================================================================================================================
+    //constructor
+    public RAM(RAM type, RAM volume, RAM weight){
+        this.type = type;
+        this.volume = volume;
+        this.weight = weight;
+    }
+}
