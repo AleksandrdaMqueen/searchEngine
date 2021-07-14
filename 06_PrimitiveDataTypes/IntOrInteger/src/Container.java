c83716441779ec0250b0600f0d8ee61a102b292c
Александр Банкевич <aleksandr@bankevich.com>
2021-07-14 21:01:07 +0000

    Update Container.java

--- 06_PrimitiveDataTypes/IntOrInteger/src/Container.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Container.java
@@ -1,6 +1,15 @@
 public class Container {
     private Integer count;
 
+    public Container(Integer count){
+        this.count = count;
+    }
+
+    public void setCount(int count){
+        this.count = count;
+    }
+
+
     public void addCount(int value) {
         count = count + value;
     }
