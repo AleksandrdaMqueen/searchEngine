03098db979dd11a16a36c5d39403c3562f269cc0
Александр Банкевич <aleksandr@bankevich.com>
2021-07-20 13:37:22 +0000

    Update Main.java

--- 07_NumbersAndDates/Date/src/main/java/Main.java
+++ 07_NumbersAndDates/Date/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 
@@ -9,14 +11,31 @@ public class Main {
 
         System.out.println(collectBirthdays(year, month, day));
 
+
     }
+
 
     public static String collectBirthdays(int year, int month, int day) {
+        LocalDate birthday = LocalDate.of(year, month, day);
+        LocalDate today = LocalDate.now();
+        String text = "";
+        int count = 0;
 
-        //TODO реализуйте метод для построения строки в следующем виде
-        //0 - 31.12.1990 - Mon
-        //1 - 31.12.1991 - Tue
-        
-        return "";
+        while (today.isAfter(birthday) || today.isEqual(birthday)) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy - E");
+
+            text += count + " - " + formatter.format(birthday) + System.lineSeparator();
+
+            birthday = birthday.plusYears(1);
+            count++;
+
+        }
+        return text;
+
+//TODO реализуйте метод для построения строки в следующем виде
+//0 - 31.12.1990 - Mon
+//1 - 31.12.1991 - Tue
+
     }
+
 }
