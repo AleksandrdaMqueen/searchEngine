1f4fe8e9c958ce4c088f27b6fbf9972276f75997
Александр Банкевич <aleksandr@bankevich.com>
2021-07-27 17:04:17 +0000

    Update Main.java

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -2,7 +2,47 @@ public class Main {
 
   public static void main(String[] args) {
 
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+    Stringpublic class Main {
+
+    public static void main(String[] args) {
+
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        //TODO: напишите ваш код, результат вывести в консоль
+        int vasya = Integer.parseInt(getVasyaEarnings(text));
+        int petya = Integer.parseInt(getPetyasEarnings(text));
+        int masha = Integer.parseInt(getMashasEarnings(text));
+        System.out.println(vasya + petya + masha);
+
+
+    }
+
+    public static String getVasyaEarnings(String text) {
+        String charsetStr = "заработал ";
+        String charset = "000";
+        int end = text.indexOf(charset) + charset.length();
+        int start = text.indexOf(charsetStr) + charsetStr.length();
+        String encoding = text.substring(start, end);
+        return encoding;
+    }
+
+    public static String getPetyasEarnings(String text) {
+        String charsetStr = "Петя - ";
+        String charset = "563";
+        int end = text.indexOf(charset) + charset.length();
+        int start = text.indexOf(charsetStr) + charsetStr.length();
+        String endcoding = text.substring(start, end);
+        return endcoding;
+    }
+
+    public static String getMashasEarnings(String text) {
+        String charsetStr = " а Маша - ";
+        String charset = "0000";
+        int end = text.indexOf(charset) + charset.length();
+        int start = text.indexOf(charsetStr) + charsetStr.length();
+        String endcoding = text.substring(start, end);
+        return endcoding;
+    }
+} text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
     //TODO: напишите ваш код, результат вывести в консоль
   }
 
