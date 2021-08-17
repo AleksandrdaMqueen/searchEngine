4e6e62e5ca1478fe2096ef4cab72f4c642fc0a84
Александр Банкевич <aleksandr@bankevich.com>
2021-08-17 15:41:37 +0000

    Update Main.java

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -4,17 +4,17 @@ public class Main {
 
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
         //TODO: напишите ваш код, результат вывести в консоль
-        nums(text);
+        System.out.println(nums(text));
+
     }
     public static   String nums (String text){
-            String rub =  "руб";
-            String name = "Вася";
-            int end = text.indexOf(rub);
-            int start = text.indexOf(name);
-            while(start != -1){
-                String nums = text.substring(start , end);
-                return nums;
-            }
+            String rub =  " руб";
+            String name = " 5";
+            int mid = text.indexOf(name);
+            int start = text.indexOf(rub);
+            int end = text.lastIndexOf(' ', mid);
+            String encoding = text.substring(end , start) ;
+            return encoding;
         }
 
 
