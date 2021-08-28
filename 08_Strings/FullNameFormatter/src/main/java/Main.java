2119312da8aa6e65e42bf81a39f6d27fbec609d4
Александр Банкевич <aleksandr@bankevich.com>
2021-08-28 14:31:49 +0000

    Update Main.java

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -3,64 +3,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        while (true) {
-            String input = scanner.nextLine();
-            String space = " ";
-            int pos = 0;
 
-            int checkSpace = input.indexOf(space);
-            int i = 0;
-            char num = input.charAt(i);
+        String input = scanner.nextLine();
+        int pos = 0;
+        int i = 0;
 
-            while (Character.isDigit(num)) {
-                i++;
-                if (Character.isDigit(num)) {
+        while (i < input.length()) {
+            i++;
+            char ch = input.charAt(i);
+
+
+            if (ch == ' ') {
+                pos++;
+                if (pos < 2) {
                     System.out.println("Введенная строка не является ФИО");
+                    break;
                 }
-
-                break;
             }
-            if (input.equals("0")) {
-                break;
-            }
-            if (input.isBlank()) {
-                System.out.println("Введенная строка не является ФИО");
-
-                break;
-            }
-            if (checkSpace == -1) {
+            if (!Character.UnicodeBlock.of(ch).equals(Character.UnicodeBlock.CYRILLIC)) {
                 System.out.println("Введенная строка не является ФИО");
                 break;
-            }if(!Character.isDigit(num))
-                while (true) {
-                    i++;
-                    if (Character.isDigit(num)) {
-                        System.out.println("Введенная строка не является ФИО");
-                    }
-
-                break;
-
-
-
             }
-            else {
-                int start = input.indexOf(' ', pos);
-                String lastname = "Фамилия: ".concat(input.substring(0, start));
-                System.out.println(lastname);
-                int c = input.substring(0,start).length();
-                int start2 = input.indexOf(' ', c + 1);
-                String name = "Имя:".concat( input.substring(c, start2));
-                System.out.println(name);
-                int a = c + input.substring(c, start2).length();
-                int start3 = input.indexOf(' ' , a);
-                String patronymic = "Отчество: ".concat(input.substring(a ,start3));
-                System.out.println(patronymic);
-
-
-            }
-
 
         }
     }
+}
