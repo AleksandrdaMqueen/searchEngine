e63f7149d3d1303603c3bb6f1337065bc5aa33c7
Александр Банкевич <aleksandr@bankevich.com>
2021-08-31 15:45:33 +0000

    Update Main.java

--- 08_Strings/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,11 +1,30 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
   public static void main(String[] args) {
-
+    calculateSalarySum("Толя заработал 35000 рублей, а Лера - 45000 рублей");
   }
 
   public static int calculateSalarySum(String text){
     //TODO: реализуйте метод
+
+    String regex ="[^А-Я][0-9]";
+
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(text);
+    while (matcher.find()){
+      int start = matcher.start();
+      int end = matcher.end();
+
+      String num =text.substring(start  ,end );
+      System.out.print(num);
+      int endSpace =num.indexOf(' ',1);
+      String firstNum = num.substring(1,endSpace);
+
+    }
+
     return -1;
   }
 
