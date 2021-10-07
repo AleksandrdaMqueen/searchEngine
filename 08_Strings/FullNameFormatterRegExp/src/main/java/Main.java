7a07264ef093d1df11c5600215b31159936bd725
Александр Банкевич <aleksandr@bankevich.com>
2021-10-07 17:01:32 +0000

    Update Main.java

--- 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
+++ 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
@@ -2,17 +2,29 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            String letterList = "[А-я]+";
+            String regex = letterList + "[\\s]" + letterList + "[\\s]" + letterList;
+            if (!input.matches(regex)) {
+                System.out.println("Введенная строка не является ФИО");
+                break;
+            }
+
+            String[] formatter = input.split("\\s");
+            System.out.println("Фамилия: " + formatter[0]);
+            System.out.println("Имя: " + formatter[1]);
+            System.out.println("Отчество: " + formatter[2]);
+
+
+            //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        }
     }
-  }
 
 }
