f18bd62f270722c645dfd391b544a342de92d09d
Александр Банкевич <aleksandr@bankevich.com>
2021-10-12 14:03:37 +0000

    Update Main.java

--- 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
+++ 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
@@ -1,7 +1,9 @@
-
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+
 
     public static void main(String[] args) {
 
@@ -11,21 +13,19 @@ public class Main {
             if (input.equals("0")) {
                 break;
             }
-            String letterList = "[А-Я][а-я]+";
-            String regex =  letterList + "-?" + letterList + "[\\s]" + letterList + "[\\s]" + letterList;
-            if (!input.matches(regex)) {
+
+            final String regex = "([А-Я][а-я]+-?[А-Я]?[а-я]+)" + "\\s" + "([А-Я][а-я]+)" + "\\s" + "([А-Я][а-я]+)";
+            final Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(input);
+            if (!matcher.matches()) {
                 System.out.println("Введенная строка не является ФИО");
                 break;
             }
 
-            String[] formatter = input.split("\\s");
-            System.out.println("Фамилия: " + formatter[0]);
-            System.out.println("Имя: " + formatter[1]);
-            System.out.println("Отчество: " + formatter[2]);
-
-
-            //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+            System.out.println("Фамилия: " + matcher.group(1));
+            System.out.println("Имя: " + matcher.group(2));
+            System.out.println("Отчество: " + matcher.group(3));
         }
-    }
 
+    }
 }
