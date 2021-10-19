c56b13df4de88003d6f3d91c864e0507dddbd501
Александр Банкевич <aleksandr@bankevich.com>
2021-10-19 14:27:38 +0000

    Update TwoDimensionalArray.java

--- 09_ArraysAndCollections/homework_9.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
+++ 09_ArraysAndCollections/homework_9.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
@@ -1,7 +1,7 @@
 public class TwoDimensionalArray {
-    public static char symbol = 'X';
 
     public static char[][] getTwoDimensionalArray(int size) {
+        char[][] figure = new char[size][size];
 
         //TODO: Написать метод, который создаст двумерный массив char заданного размера.
         // массив должен содержать символ symbol по диагоналям, пример для size = 3
@@ -9,6 +9,25 @@ public class TwoDimensionalArray {
         // [ , X,  ]
         // [X,  , X]
 
-        return new char[0][0];
+        for (int x = 0; x < figure.length; x++) {
+            for (int y = 0; y < figure.length; y++) {
+                if (x == y) {
+                    figure[x][y] = 'X';
+                } else {
+                    figure[x][y] = ' ';
+                }
+            }
+        }
+        for (int j = figure.length; j > 0; j--) {
+            for (int i = figure.length; i > 0; i--) {
+                if (j == i) {
+                    figure[j][i] = 'X';
+                } else {
+                    figure[j][i] = ' ';
+                }
+            }
+        }
+
+        return  figure;
     }
 }
