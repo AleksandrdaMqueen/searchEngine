3110b079bfb92251b1ac4c0c36e0e2f503c79b20
Александр Банкевич <aleksandr@bankevich.com>
2021-10-30 21:35:30 +0000

    Update Main.java

--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String myLine = scan.nextLine();
         // TODO: написать консольное приложение для работы со списком дел todoList
+        int end = myLine.indexOf(" ",0);
+        String typeOfOperation = myLine.substring(0,end);
+        int start = myLine.indexOf(' ', 1);
+        int end1 =  myLine.length();
+        String todo = myLine.substring(start,end1);
+        if(typeOfOperation == "ADD"){
+            todoList.add(todo);
+        }
+
     }
 }
