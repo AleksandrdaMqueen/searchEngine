1d459ab79e8f8f00ce565970d18e346d1143dcdd
Александр Банкевич <aleksandr@bankevich.com>
2021-11-09 14:45:06 +0000

    Update Main.java

--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     private static TodoList todoList = new TodoList();
@@ -9,41 +11,13 @@ public class Main {
             String myLine = scan.nextLine();
 
 
-            int end = myLine.indexOf(" ", 0);
-
-            int end1 = myLine.length();
-            int start = myLine.indexOf(' ', end);
-            int length = myLine.length();
-
-            String typeOfOperation = myLine.substring(0, end);
-            String regex = "(ADD|DELETE|EDIT|LIST)\\s(0-9)+\\s(A-z)+";
-            String num = myLine.substring(end, start);
-
-
-
-            if (typeOfOperation.matches(regex)) {
-                String todoWithIndex = myLine.substring(start, end1);
-                int index = Integer.parseInt(num);
-                switch (typeOfOperation) {
-                    case "ADD":
-                        todoList.add(index, todoWithIndex);
-                        break;
-                    case "DELETE":
-                        todoList.delete(index);
-                        break;
-                    case "EDIT":
-                        todoList.edit(todoWithIndex, index);
-                        break;
-                }
+            String regex = "(ADD|DELETE|EDIT|LIST)\\s?([0-9]+)?\\s?([A-z]+)?";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(myLine);
+            if (!matcher.matches()) {
+                System.out.println("Incorrect expression");
             }
-            else if(!typeOfOperation.matches(regex)){
-                String todoWithoutIndex = myLine.substring(end, length);
-                if ("ADD".equals(typeOfOperation)) {
-                    todoList.add(todoWithoutIndex);
-                }
-
-            }
-
+            System.out.println("Command: " + matcher.group(1) + " Index: " + matcher.group(2) + " Text: " + matcher.group(3));
         }
     }
 }
