f4d235dc6e80b66c8e3c7b53b9216f0b18ca6679
Александр Банкевич <aleksandr@bankevich.com>
2021-11-09 14:44:33 +0000

    Update TodoList.java

--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/TodoList.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/TodoList.java
@@ -16,37 +16,54 @@ public class TodoList {
     public void add(int index, String todo) {
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
-        for (int i = 0; i < whatTodo.size(); i++) {
 
 
-            if (index > i || index < 0) {
-                System.out.println("Невозможно добавить дело с таким индесом");
-                break;
+
+            if (index < 0 ||  index + 1 > whatTodo.size()) {
+                add(todo);
+
             } else {
                 whatTodo.add(index, todo);
                 System.out.println("Добавлено дело: " + todo + "На место: " + index);
             }
-        }
+
     }
 
     public void edit(String todo, int index) {
         // TODO: заменить дело на index переданным todo индекс,
+
+
+            if ( index < 0 || index + 1> whatTodo.size()) {
+                System.out.println("Невозможно добавить дело с таким индесом");
+
+            } else {
+                whatTodo.remove(index);
+                whatTodo.add(index, todo);
+                System.out.println("Добавлено дело: " + todo + "На место: " + index);
+            }
+
         //  проверьте возможность изменения
     }
 
     public void delete(int index) {
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
-        for (int i = 0; i < whatTodo.size(); i++) {
-            if (index > i || index < 0) {
+            if (index < 0 ||  index + 1 > whatTodo.size()) {
                 System.out.println("Невозможно удалить дело с таким индесом");
-                break;
             } else {
                 whatTodo.remove(index);
             }
 
-        }
 
+
+    }
+
+    public void list(ArrayList<String> whatTodo){
+        for (int i = 0; i < whatTodo.size() - 1; i++) {
+            for (String item : whatTodo) {
+                System.out.println(i +"\\s" + item);
+            }
+        }
     }
 
     public ArrayList<String> getTodos() {
