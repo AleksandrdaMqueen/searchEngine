48c6dd924ecacd0fedc6576f3e9e4b7b9c5b7dfa
Александр Банкевич <aleksandr@bankevich.com>
2021-11-16 14:33:26 +0000

    Update Main.java

--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
@@ -4,13 +4,15 @@ import java.util.regex.Pattern;
 
 public class Main {
     private static final TodoList todoList = new TodoList();
+    private static final Pattern p = Pattern.compile("(ADD|LIST|EDIT|DELETE)\\s*([0-9]*)\\s*(.+)*");
+
 
     public static void main(String[] args) {
         while (true) {
             Scanner scan = new Scanner(System.in);
             String myLine = scan.nextLine();
 
-            final Pattern p = Pattern.compile("(ADD|LIST|EDIT|DELETE)\\s*([0-9]*)\\s*(.+)*");
+
             Matcher m = p.matcher(myLine);
             if (!m.matches()) {
                 System.out.println("Некорректный формат команды");
@@ -19,8 +21,8 @@ public class Main {
             String command = m.group(1);
             int index = !m.group(2).isBlank() ? Integer.parseInt(m.group(2)) : -1;
             String text = m.group(3);
-            if(index != -1 && command.equals("ADD")){
-                todoList.add(index,text);
+            if (index != -1 && command.equals("ADD")) {
+                todoList.add(index, text);
                 continue;
             }
 
@@ -33,7 +35,7 @@ public class Main {
 
                     String resultOfMethod = todoList.edit(text, index);
                     if (resultOfMethod != null) {
-                        System.out.println("Дело "+ resultOfMethod + " удаленно " );
+                        System.out.println("Дело " + resultOfMethod + " удаленно ");
                     } else {
                         System.out.println("Невозможно удалить дело с таким индесом");
                     }
@@ -42,13 +44,16 @@ public class Main {
                 case "EDIT":
                     String result = todoList.edit(text, index);
                     if (result != null) {
-                        System.out.println("Дело "+ result + " заменено на " + text);
+                        System.out.println("Дело " + result + " заменено на " + text);
                     } else {
                         System.out.println("Невозможно добавить дело с таким индесом");
                     }
                     break;
                 case "LIST":
-                    todoList.list();
+                    todoList.getTodos();
+                    for (int i = 0; i < todoList.getTodos().size(); i++) {
+                        System.out.println(i + " - " + todoList.getTodos().get(i));
+                    }
             }
         }
     }
