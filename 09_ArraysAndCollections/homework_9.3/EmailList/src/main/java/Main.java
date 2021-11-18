c37df1b70bc8caae6513feed13e99e9446785ee4
Александр Банкевич <aleksandr@bankevich.com>
2021-11-18 15:49:41 +0000

    Update Main.java

--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
@@ -1,8 +1,12 @@
+
 import java.util.Scanner;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
-    
+
     /* TODO:
         Пример вывода списка Email, после ввода команды LIST в консоль:
         test@test.com
@@ -19,15 +23,34 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        final TreeSet<String> emailList = new TreeSet<>();
+        String regex = "(LIST|ADD)\\\\s?(.*)";
+
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("0")) {
                 break;
+            } else {
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(input);
+                String emailAdress = matcher.group(2).toLowerCase();
+                String command = matcher.group(1);
+                if (!matcher.matches()) {
+                    System.out.println(WRONG_EMAIL_ANSWER);
+                    break;
+                }
+                switch (command){
+                    case "ADD":
+                        emailList.add(emailAdress);
+                    case "LIST":
+                        for (String emails : emailList) {
+                                System.out.println(emails);
+                            }
+                    }
+
+
+
             }
-            
-            //TODO: write code here
-            
         }
     }
 }
