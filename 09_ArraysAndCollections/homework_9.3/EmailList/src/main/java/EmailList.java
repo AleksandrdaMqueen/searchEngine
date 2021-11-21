fb1153d1e2769a443293169f42c87e139cc88034
Александр Банкевич <aleksandr@bankevich.com>
2021-11-21 16:50:35 +0000

    Update EmailList.java

--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
@@ -7,17 +7,18 @@ public class EmailList {
     private final TreeSet<String> emailList = new TreeSet<>();
 
     public void add(String email) {
-        String regex = "([A-z]+)@([A-z]+)\\.([a-z]+)";
+        String regex = "([A-z]+@[A-z]+\\.[a-z]+)";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(email.toLowerCase(Locale.ROOT));
+        Matcher matcher = pattern.matcher(email);
         if (!matcher.matches()) {
             System.out.println("Неверный формат email");
+        } else if (emailList.contains(email.toLowerCase(Locale.ROOT))) {
+            System.out.println("Такой email уже существует");
         } else {
             emailList.add(email);
             System.out.println("Добавлен email " + email);
         }
     }
-
 
 
     public List<String> getSortedEmails() {
