d5dc36ac7e481f5428b4f3b4a0d3943dfb344e46
Александр Банкевич <aleksandr@bankevich.com>
2021-11-28 16:20:11 +0000

    Update PhoneBook.java

--- 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/PhoneBook.java
+++ 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/PhoneBook.java
@@ -1,65 +1,75 @@
-mport java.util.*;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PhoneBook {
-    private  final HashMap<String, String> phones = new HashMap<>();
+    private final HashMap<String, String> phones = new HashMap<>();
+    private final String regex4name = "([А-я]+)";
+    private final String regex4phone = "([0-9]{10,11})";
+
+    private final Pattern pattern4name = Pattern.compile(regex4name);
+    private final Pattern pattern4phone = Pattern.compile(regex4phone);
+
 
     public void addContact(String phone, String name) {
-        String regex4name = "([А-я]+)";
-        String regex4phone = "([0-9]{10,11})";
 
-        Pattern pattern4name = Pattern.compile(regex4name);
-        Matcher matcher4name = pattern4name.matcher(phone);
 
-        Pattern pattern4phone = Pattern.compile(regex4phone);
+        Matcher matcher4name = pattern4name.matcher(name);
         Matcher matcher4phone = pattern4phone.matcher(phone);
-        if (!matcher4phone.matches() || !matcher4name.matches() ) {
+
+
+
+        if (!matcher4phone.matches() || !matcher4name.matches()  ) {
             System.out.println("Невалидный  ввод");
+
         } else {
-            phones.put(phone, name);
+            phones.put(name, phone);
         }
         // проверьте корректность формата имени и телефона (отдельные методы для проверки)
         // если такой номер уже есть в списке, то перезаписать имя абонента
     }
 
     public String getContactByPhone(String phone) {
-        for (int i = 0; i < phones.size(); i++) {
+        String name = "";
 
-            if (phones.containsValue(phone)) {
-                return phones.get(phone);
-
+        if (phones.containsValue(phone)) {
+            for (String key : phones.keySet()) {
+                name += key  + " - " + phones.get(key);
             }
+
+            return name;
+        }else {
+            return "";
         }
         // формат одного контакта "Имя - Телефон"
         // если контакт не найдены - вернуть пустую строку
-        return "";
+
     }
 
     public Set<String> getContactByName(String name) {
-        TreeSet<String> contactByName= new TreeSet<>();
-        if (phones.containsValue(name)) {
-            for(String key : phones.keySet()){
-                if(Objects.equals(phones.get(key), name)){
-                    contactByName.add(key);
-                }
+        TreeSet<String> contactByName = new TreeSet<>();
+
+        if (phones.containsKey(name)) {
+            for (String key : phones.keySet()) {
+                contactByName.add(key + " - " + phones.get(name));
             }
 
             return contactByName;
+        }else {
+            return new TreeSet<>();
         }
-        return new TreeSet<>();
         // формат одного контакта "Имя - Телефон"
         // если контакт не найден - вернуть пустой TreeSet
 
     }
 
     public Set<String> getAllContacts() {
-        TreeSet<String> phoneList = new  TreeSet<>();
+        TreeSet<String> phoneList = new TreeSet<>();
         if (phones != null) {
-            for (Map.Entry<String, String> entry : phones.entrySet()){
+            for (Map.Entry<String, String> entry : phones.entrySet()) {
                 String key = entry.getKey(); // получения ключа
                 String value = entry.getValue(); // получения ключа
-                phoneList.add(key + " - "+value);
+                phoneList.add(key + " - " + value);
             }
             return phoneList;
 
