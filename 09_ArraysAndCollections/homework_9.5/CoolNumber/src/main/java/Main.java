42fc0a0ac499171331f991939b69525e934aa9e9
Александр Банкевич <aleksandr@bankevich.com>
2021-12-11 15:57:27 +0000

    Update Main.java

--- 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
@@ -1,4 +1,8 @@
+import java.util.*;
+
 public class Main {
+
+
     /*
     TODO:
      - реализовать методы класса CoolNumbers
@@ -7,6 +11,58 @@ public class Main {
      */
 
     public static void main(String[] args) {
+        ArrayList<String> numbers = new ArrayList<>();
+        HashSet<String> numbers1 = new HashSet<>();
+        TreeSet<String> numbers2  = new TreeSet<>();
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String num = scanner.nextLine();
+            System.out.println("Введите искомый номер:");
+
+            numbers.generateCoolNumbers();
+
+
+            long startTime2 = System.nanoTime();
+            boolean isContains = CoolNumbers.bruteForceSearchInList(numbers, num);
+            long endTime2 = System.nanoTime();
+            if(isContains){
+                System.out.println("Поиск номера перебором заял: " + (endTime2 - startTime2) + "ns");
+            }else{
+                System.out.println("Номер не найден");
+            }
+
+
+
+            long startTime3 = System.nanoTime();
+            boolean isContains2 = CoolNumbers.binarySearchInList(numbers, num);
+            long endTime3 = System.nanoTime();
+            if(isContains2){
+                System.out.println("Поиск номера перебором заял: " + (endTime3 - startTime3) + "ns");
+            }else{
+                System.out.println("Номер не найден");
+            }
+
+            long startTime4 = System.nanoTime();
+            boolean isContains3 = CoolNumbers.searchInHashSet(numbers1, num);
+            long endTime4 = System.nanoTime();
+            if(isContains3){
+                System.out.println("Поиск номера перебором заял: " + (endTime4 - startTime4) + "ns");
+            }else{
+                System.out.println("Номер не найден");
+            }
+
+            long startTime5 = System.nanoTime();
+
+            long endTime5 = System.nanoTime();
+            boolean isContains4 = CoolNumbers.searchInTreeSet(numbers2, num);
+            if(isContains4){
+                System.out.println("Поиск номера перебором заял: " + (endTime5 - startTime5) + "ns");
+            }else{
+                System.out.println("Номер не найден");
+            }
+
+
+        }
 
     }
 }
