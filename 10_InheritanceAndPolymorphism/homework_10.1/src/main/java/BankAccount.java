2612af56ba0ee58dc7d1141966637ee525fa4948
Александр Банкевич <aleksandr@bankevich.com>
2021-12-16 14:28:43 +0000

    Update BankAccount.java

--- 10_InheritanceAndPolymorphism/homework_10.1/src/main/java/BankAccount.java
+++ 10_InheritanceAndPolymorphism/homework_10.1/src/main/java/BankAccount.java
@@ -1,18 +1,26 @@
 public class BankAccount {
+  public double totalMoney = 0;
 
-  public double getAmount() {
-    //TODO: реализуйте метод и удалите todo
-    // верните значение количества денег не счету
-    return 0;
-  }
 
   public void put(double amountToPut) {
-    //TODO: реализуйте метод и удалите todo
-    // метод зачисляет деньги на счет
+    if( amountToPut < 0){
+      System.out.println("Невалидная сумма денег");
+    }else {
+      totalMoney += amountToPut;
+    }
   }
 
   public void take(double amountToTake) {
-    //TODO: реализуйте метод и удалите todo
-    // метод списывает деньги со счета
+    if(amountToTake > totalMoney ){
+      System.out.println("Недостаточно денег на счету");
+    }else {
+      totalMoney -= amountToTake;
+    }
+
+  }
+
+  public double getAmount() {
+
+    return totalMoney;
   }
 }
