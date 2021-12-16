b1d0b22c1b230a37f2d1f2498c89cdd9e955dbea
Александр Банкевич <aleksandr@bankevich.com>
2021-12-16 14:29:09 +0000

    Update CardAccount.java

--- 10_InheritanceAndPolymorphism/homework_10.1/src/main/java/CardAccount.java
+++ 10_InheritanceAndPolymorphism/homework_10.1/src/main/java/CardAccount.java
@@ -1,4 +1,8 @@
 public class CardAccount extends BankAccount {
-    // не забывайте, обращаться к методам и конструкторам родителя
-    // необходимо используя super, например, super.put(10D);
+
+    public void take(double amountToTake) {
+        super.take(amountToTake + (amountToTake / 100 * 1));
+
+    }
+    
 }
