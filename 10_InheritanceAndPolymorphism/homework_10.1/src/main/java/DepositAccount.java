ac813230b3dc80227ffc535d33d4ae637bac417e
Александр Банкевич <aleksandr@bankevich.com>
2021-12-16 14:30:33 +0000

    Update DepositAccount.java

--- 10_InheritanceAndPolymorphism/homework_10.1/src/main/java/DepositAccount.java
+++ 10_InheritanceAndPolymorphism/homework_10.1/src/main/java/DepositAccount.java
@@ -1,3 +1,20 @@
+import java.time.LocalDate;
+
 public class DepositAccount extends BankAccount {
 
+    public static boolean isMonthAgo(){
+
+         LocalDate lastInCome = LocalDate.now();
+  
+        return false;
+    }
+
+    @Override
+    public void take(double amountToTake) {
+        if(isMonthAgo() && amountToTake < totalMoney) {
+            super.take(amountToTake);
+        }else{
+            System.out.println("Еще не прошел месяц");
+        }
+    }
 }
