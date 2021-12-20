a89d316c05a29bc94892f1cfbd0bc01e869b2c0f
Александр Банкевич <aleksandr@bankevich.com>
2021-12-20 13:16:08 +0000

    Update IndividualBusinessman.java

--- 10_InheritanceAndPolymorphism/homework_10.3/src/main/java/IndividualBusinessman.java
+++ 10_InheritanceAndPolymorphism/homework_10.3/src/main/java/IndividualBusinessman.java
@@ -1,2 +1,15 @@
-public class IndividualBusinessman extends Client {
+public class IndividualBusinessman extends Client  {
+    @Override
+    public void put(double amountToPut) {
+        if(amountToPut < 1000) {
+            super.put(amountToPut - (amountToPut  / 100 * 1));
+        }else if (amountToPut >= 1000){
+            super.put(amountToPut -( amountToPut  / 100 * 0.5));
+        }
+    }
+
+    @Override
+    public void take(double amountToTake) {
+        super.take(amountToTake);
+    }
 }
