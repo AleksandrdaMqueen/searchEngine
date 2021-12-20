72c2cbf051770c9b4cb66b6dc07fed6c006c2b88
Александр Банкевич <aleksandr@bankevich.com>
2021-12-20 13:16:39 +0000

    Update PhysicalPerson.java

--- 10_InheritanceAndPolymorphism/homework_10.3/src/main/java/PhysicalPerson.java
+++ 10_InheritanceAndPolymorphism/homework_10.3/src/main/java/PhysicalPerson.java
@@ -1,2 +1,11 @@
 public class PhysicalPerson extends Client {
+    @Override
+    public void take(double amountToTake) {
+        super.take(amountToTake);
+    }
+
+    @Override
+    public void put(double amountToPut) {
+        super.put(amountToPut);
+    }
 }
