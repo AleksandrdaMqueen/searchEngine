35dc4e546fb26d2c3496edcdadebc066549f5794
Александр Банкевич <aleksandr@bankevich.com>
2021-12-20 13:16:25 +0000

    Update LegalPerson.java

--- 10_InheritanceAndPolymorphism/homework_10.3/src/main/java/LegalPerson.java
+++ 10_InheritanceAndPolymorphism/homework_10.3/src/main/java/LegalPerson.java
@@ -1,2 +1,10 @@
 public class LegalPerson extends Client {
+    public void take(double amountToTake) {
+        super.take(amountToTake + amountToTake * 1 / 100);
+    }
+
+    @Override
+    public void put(double amountToPut) {
+        super.put(amountToPut);
+    }
 }
