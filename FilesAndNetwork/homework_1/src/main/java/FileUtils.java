191d9098eedd35585a78495f8de1dc950f1f552c
Александр Банкевич <aleksandr@bankevich.com>
2022-01-26 14:30:17 +0000

    Update FileUtils.java

--- FilesAndNetwork/homework_1/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_1/src/main/java/FileUtils.java
@@ -1,6 +1,22 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        Long size = 0L;
+        Path folder =  Paths.get(path);
+        try {
+             size = Files.walk(folder)
+                    .map(Path::toFile)
+                    .filter(File::isFile)
+                    .mapToLong(File::length)
+                    .sum();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return size;
     }
 }
