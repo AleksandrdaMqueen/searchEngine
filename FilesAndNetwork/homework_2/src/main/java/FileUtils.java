b5efe6ce312fa86f9238b038dba6cd40f3a5e9db
Александр Банкевич <aleksandr@bankevich.com>
2022-01-26 19:13:26 +0000

    Update FileUtils.java

--- FilesAndNetwork/homework_2/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_2/src/main/java/FileUtils.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.io.PrintWriter;
+
+
 public class FileUtils {
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+        try {
+
+
+            File filesFromSrcDirectory = new File(sourceDirectory);
+            File[] files = filesFromSrcDirectory.listFiles();
+            PrintWriter printWriter = new PrintWriter(destinationDirectory);
+
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].isFile()){
+                    printWriter.write(files[i].getName());
+                }
+            }
+        }catch (Exception exception){
+            exception.printStackTrace();
+        }
     }
 }
+//TODO: write code copy content of sourceDirectory to destinationDirectory;
