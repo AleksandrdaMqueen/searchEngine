442cf1e5ea322ab4cccc71238bab7e48fe104590
Александр Банкевич <aleksandr@bankevich.com>
2022-02-08 09:49:41 +0000

    Update Main.java

--- FilesAndNetwork/lenta/src/Main.java
+++ FilesAndNetwork/lenta/src/Main.java
@@ -2,47 +2,48 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> images = new ArrayList<>();
-
-        String htmFile = parseFile("C:\\Users\\abake\\IdeaProjects\\hw_3\\hw_4\\data\\code.html");
-
-        Document document = Jsoup.parse(htmFile);
-
-        Elements elements = document.select("img");
-
-        elements.forEach(element -> System.out.println(element.absUrl("src")));
-        elements.forEach(element -> images.add(element.absUrl("src")));
         try {
-            Files.write(Paths.get("C:\\Users\\abake\\IdeaProjects\\hw_3\\hw_4\\data\\images.txt"), images);
-        }catch (Exception e){
+            ArrayList<String> images = new ArrayList<>();
+
+            String htmFile = parseFile("C:\\Users\\abake\\IdeaProjects\\hw_3\\hw_4\\data\\code.html");
+
+            Document document = Jsoup.parse(htmFile);
+
+            Elements elements = document.select("img");
+
+            elements.forEach(element -> System.out.println(element.absUrl("src")));
+            elements.forEach(element -> images.add(element.absUrl("src")));
+
+            for (int i = 0; i < images.size(); i++) {
+
+
+                InputStream in = new URL(images.get(i)).openStream();
+                Files.copy(in, Paths.get("C:\\Users\\abake\\IdeaProjects\\hw_3\\hw_4\\data\\image"+ i + ".jpg"  ));
+            }
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        String command = scanner.nextLine();
-
-
-
     }
-
-    public static String parseFile(String path){
+    public static String parseFile(String path) {
         StringBuilder stringBuilder = new StringBuilder();
         try {
             List<String> lines = Files.readAllLines(Paths.get(path));
             lines.forEach(line -> stringBuilder.append(line + '\n'));
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
-
 
 
         return stringBuilder.toString();
