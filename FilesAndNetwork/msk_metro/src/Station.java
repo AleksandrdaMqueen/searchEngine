74b4af7f009657f07e605ec0401674b8bd3da718
Александр Банкевич <aleksandr@bankevich.com>
2022-02-12 19:12:52 +0000

    Update Station.java

--- FilesAndNetwork/msk_metro/src/Station.java
+++ FilesAndNetwork/msk_metro/src/Station.java
@@ -1,1 +1,16 @@
+public class Station {
+    private  String name;
+    private  String lineNum;
+    public Station( String lineNum,String name){
+        this.lineNum = lineNum;
+        this.name = name;
 
+    }
+
+    @Override
+    public String toString() {
+        return
+                  name + '\t'
+                ;
+    }
+}
