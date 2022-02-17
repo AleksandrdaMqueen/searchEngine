8dd5c0a85ed09565e68fa37a787fcb2ac309fb5d
Александр Банкевич <aleksandr@bankevich.com>
2022-02-17 17:53:57 +0000

    Update Main.java

--- SQLAndHibernate/config/SQL/java.java/Main.java
+++ SQLAndHibernate/config/SQL/java.java/Main.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,30 +10,40 @@ public class Main {
         String user = "root";
         String pass = "polar12Bear";
         try {
-            int lastNum = 0;
+            int months = 0;
             int count = 0;
-
+            ArrayList<String> arrayOfNames = new ArrayList<>();
 
             Connection connection = DriverManager.getConnection(url, user, pass);
 
             Statement statement = connection.createStatement();
             Statement statement1 = connection.createStatement();
             Statement statement2 = connection.createStatement();
+            Statement statement3 = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT subscription_date as date , course_name as name FROM purchaselist WHERE YEAR(subscription_date) = 2018 ORDER BY subscription_date");
 
-            ResultSet getLastNum = statement1.executeQuery("SELECT MONTH(subscription_date) as date FROM purchaselist WHERE YEAR(subscription_date) = 2018 ORDER BY subscription_date ");
-
-            while (resultSet.next()) {
-                count++;
-                resultSet.getString("name");
-                resultSet.getString("date");
+            ResultSet getLastNum = statement1.executeQuery("SELECT MONTH(subscription_date) as date FROM purchaselist WHERE YEAR(subscription_date) = 2018 ORDER BY subscription_date");
+            ResultSet name = statement2.executeQuery("SELECT  name  as name FROM courses");
+            while (name.next()) {
+                arrayOfNames.add(name.getString("name"));
             }
-            while (getLastNum.next()){
-                lastNum = getLastNum.getInt("date");
+            while (getLastNum.next()) {
+                months = getLastNum.getInt("date");
 
             }
 
-            int result = count/lastNum;
+
+            for (int i = 0; i <= arrayOfNames.size() -1 ; i++) {
+                String names = arrayOfNames.get(i);
+                ResultSet resultSet1 = statement3.executeQuery("SELECT subscription_date as date from purchaselist WHERE name = " + names);
+                while (resultSet1.next()){
+                    count++;
+
+                }
+                int res = count/ months;
+                System.out.println("Средние продажи в месяц курса " + names + "равна " + res);
+
+            }
 
 
             statement.close();
@@ -42,7 +53,7 @@ public class Main {
             statement1.close();
             statement2.close();
 
-            System.out.println(result);
+
         } catch (Exception exception) {
             exception.printStackTrace();
         }
