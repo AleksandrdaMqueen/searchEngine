d32b470765e0f1095cb16b567b663aff5998c2d2
Александр Банкевич <aleksandr@bankevich.com>
2022-02-21 14:04:19 +0000

    Update Teacher.java

--- SQLAndHibernate/config/ENTITIES/JAVA/Teacher.java
+++ SQLAndHibernate/config/ENTITIES/JAVA/Teacher.java
@@ -1,1 +1,50 @@
+import javax.persistence.*;
 
+@Entity
+@Table(name = "Teachers")
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private String name;
+
+    private int salary;
+
+
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+}
