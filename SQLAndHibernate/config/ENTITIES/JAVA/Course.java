1bd9c141bdc9d353973f0799610b912ce3a01327
Александр Банкевич <aleksandr@bankevich.com>
2022-02-26 13:51:04 +0000

    Update Course.java

--- SQLAndHibernate/config/ENTITIES/JAVA/Course.java
+++ SQLAndHibernate/config/ENTITIES/JAVA/Course.java
@@ -1,7 +1,12 @@
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name= "courses")
+@Setter
+@Getter
 public class Course {
 
     @Id
@@ -21,84 +26,14 @@ public class Course {
     @ManyToOne(cascade = CascadeType.ALL)
     Teacher teacher;
 
-    @Column(name = "students_count")
+    @Column(name = "students_count", nullable = true)
     int studentsCount;
 
     int price;
     @Column(name = "price_per_hour")
     float pricePerHour;
 
-    public CourseType getType() {
-        return type;
-    }
 
-    public float getPricePerHour() {
-        return pricePerHour;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public int getStudentsCount() {
-        return studentsCount;
-    }
-
-    public Teacher getTeacher() {
-        return teacher;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public void setPricePerHour(float pricePerHour) {
-        this.pricePerHour = pricePerHour;
-    }
-
-    public void setStudentsCount(int studentsCount) {
-        this.studentsCount = studentsCount;
-    }
-
-    public void setTeacherId(Teacher teacher) {
-        this.teacher = teacher;
-    }
-
-    public void setType(CourseType type) {
-        this.type = type;
-    }
 
     @Override
     public String toString() {
@@ -107,3 +42,4 @@ public class Course {
 
     }
 }
+
