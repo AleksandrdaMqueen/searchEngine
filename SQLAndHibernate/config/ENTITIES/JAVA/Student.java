0344446f14956d3c5d347ab584055390d4752c7b
Александр Банкевич <aleksandr@bankevich.com>
2022-02-26 13:52:12 +0000

    Update Student.java

--- SQLAndHibernate/config/ENTITIES/JAVA/Student.java
+++ SQLAndHibernate/config/ENTITIES/JAVA/Student.java
@@ -1,50 +1,27 @@
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
 @Table(name = "students")
+@Setter
+@Getter
+@NoArgsConstructor
 public class Student {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
 
-    @ManyToOne(cascade = CascadeType.ALL)
-    private Student name;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int id;
 
-    private int age;
-    @Column(name = "registration_date")
-    private Date registrationDate;
+        private String name;
+
+        private int age;
+
+        @Column(name = "registration_date")
+        private Date registrationDate;
 
 
-    public void setAge(int age) {
-        this.age = age;
     }
-
-    public int getAge() {
-        return age;
-    }
-
-    public Student getName() {
-        return name;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(Student name) {
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public Date getRegistrationDate() {
-        return registrationDate;
-    }
-
-    public void setRegistrationDate(Date registrationDate) {
-        this.registrationDate = registrationDate;
-    }
-}
