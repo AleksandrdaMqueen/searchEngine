880022b8984f4936cf68d15bb80a1a97638dc784
Александр Банкевич <aleksandr@bankevich.com>
2022-02-26 13:50:36 +0000

    Update PurchaseList.java

--- SQLAndHibernate/config/ENTITIES/JAVA/PurchaseList.java
+++ SQLAndHibernate/config/ENTITIES/JAVA/PurchaseList.java
@@ -1,50 +1,32 @@
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.util.Date;
 @Entity
 @Table(name = "purchaselist")
-public class PurcaseList
+@Getter
+@Setter
+public class PurchaseList
 {
+    @EmbeddedId
+    private Key id;
+
     @Column(name = "student_name")
     private String studentName;
 
+    @Column(name = "course_name")
     private String courseName;
 
     private int price;
+
     @Column(name = "subscription_date")
     private Date subscriptionDate;
 
-    public void setPrice(int price) {
-        this.price = price;
-    }
 
-    public int getPrice() {
-        return price;
-    }
-
-    public Date getSubscriptionDate() {
-        return subscriptionDate;
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public String getStudentName() {
-        return studentName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
-    }
-
-    public void setSubscriptionDate(Date subscriptionDate) {
-        this.subscriptionDate = subscriptionDate;
-    }
 
 }
+
