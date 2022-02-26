5053d9d2d5dbef888ff4185f363fa15e94020455
Александр Банкевич <aleksandr@bankevich.com>
2022-02-26 13:50:11 +0000

    Update Main.java

--- SQLAndHibernate/config/ENTITIES/JAVA/Main.java
+++ SQLAndHibernate/config/ENTITIES/JAVA/Main.java
@@ -1,28 +1,71 @@
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import java.util.Scanner;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import java.util.List;
+
 
 public class Main {
+
     public static void main(String[] args) {
-        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
-        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        try {
 
-        SessionFactory factory = metadata.getSessionFactoryBuilder().build();
 
-        Session session = factory.openSession();
-        System.out.println("Введите id курса: ");
-        Scanner scanner = new Scanner(System.in);
 
-        int id = scanner.nextInt();
 
-        Course course = session.get(Course.class,  id);
-        System.out.println(course);
+            StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+            Metadata data = new MetadataSources(registry).getMetadataBuilder().build();
+
+            SessionFactory sessionFactory = data.getSessionFactoryBuilder().build();
+
+            Session session = sessionFactory.openSession();
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<PurchaseList> query = builder.createQuery(PurchaseList.class);
+
+
+            Root<PurchaseList> root = query.from(PurchaseList.class);
+            List<PurchaseList> purchaseLists = session.createQuery(query).getResultList();
+
+            query.select(root);
+
+            Transaction transaction = session.beginTransaction();
+            LinkedPurchaseList linkedPurchaseList = new LinkedPurchaseList();
+
+            purchaseLists.forEach(purchaseList -> {
+                String courseName = purchaseList.getCourseName();
+                String studentName = purchaseList.getStudentName();
+                String hql = "From" + Student.class.getSimpleName() + " As s where s.name = " + "'" + studentName +"'";
+                String hql2 = "From" + Course.class.getSimpleName() + " As c where c.name = " + "'" + courseName +"'";
+                Student student = (Student) session.createQuery(hql).getSingleResult();
+                Course course = (Course) session.createQuery(hql2).getSingleResult();
+                int studentId = student.getId();
+                int courseId = course.getId();
+                linkedPurchaseList.setStudentId(studentId);
+                linkedPurchaseList.setCourseId(courseId);
+            });
+
+
+            session.save(linkedPurchaseList);
+            transaction.commit();
+            sessionFactory.close();
+            session.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
 
     }
 
 }
+
+
