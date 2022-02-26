5b699bb7012673ab437d443b0a1dfdd7c36ba0d1
Александр Банкевич <aleksandr@bankevich.com>
2022-02-26 13:51:20 +0000

    Update Teacher.java

--- SQLAndHibernate/config/ENTITIES/JAVA/Teacher.java
+++ SQLAndHibernate/config/ENTITIES/JAVA/Teacher.java
@@ -1,51 +1,27 @@
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "Teachers")
+@Setter
+@Getter
 public class Teacher {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @OneToMany(cascade = CascadeType.ALL)
-    private Teacher name;
+    private String name;
 
     private int salary;
 
 
     private int age;
 
-    public int getId() {
-        return id;
-    }
 
-    public void setName(Teacher name) {
-        this.name = name;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Teacher getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
 
 }
+
 
