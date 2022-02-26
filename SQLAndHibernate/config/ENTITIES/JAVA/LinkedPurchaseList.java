c1e8bf28c057b0ddadd0f0d6fb41056694c742dc
Александр Банкевич <aleksandr@bankevich.com>
2022-02-26 13:52:44 +0000

    Update LinkedPurchaseList.java

--- SQLAndHibernate/config/ENTITIES/JAVA/LinkedPurchaseList.java
+++ SQLAndHibernate/config/ENTITIES/JAVA/LinkedPurchaseList.java
@@ -1,1 +1,27 @@
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+@Entity
+@Setter
+@Getter
+@Table(name = "LinkedPurchaseList")
+
+public class LinkedPurchaseList {
+    @EmbeddedId
+
+    private Key id;
+    @Column(name = "student_id", insertable = false, updatable = false,nullable = true)
+
+    private Integer studentId;
+
+    @Column(name = "course_id", insertable = false, updatable = false,nullable = true)
+
+    private Integer courseId;
+
+
+}
