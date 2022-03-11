742a8523f3ec18b6ede2d2989758d1214b2d9fe1
Александр Банкевич <aleksandr@bankevich.com>
2022-03-11 17:15:36 +0000

    Update Bank.java

--- Multithreading/Transactions/src/Bank.java
+++ Multithreading/Transactions/src/Bank.java
@@ -1,35 +1,43 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 
 public class Bank {
+    public static List<Thread> threads = new ArrayList<>();
 
-    private Map<String, Account> accounts;
+    private HashMap<String, Account> accounts = new HashMap<>();
     private final Random random = new Random();
 
+    public long getBalance(String accountNum) {
+        return accounts.get(accountNum).getMoney();
+    }
+
+    public void addAcc(String accNum, Account account) {
+        accounts.put(accNum, account);
+    }
+
     public synchronized boolean isFraud(String fromAccountNum, String toAccountNum, long amount)
-        throws InterruptedException {
+            throws InterruptedException {
         Thread.sleep(1000);
         return random.nextBoolean();
     }
 
-    /**
-     * TODO: реализовать метод. Метод переводит деньги между счетами. Если сумма транзакции > 50000,
-     * то после совершения транзакции, она отправляется на проверку Службе Безопасности – вызывается
-     * метод isFraud. Если возвращается true, то делается блокировка счетов (как – на ваше
-     * усмотрение)
-     */
     public void transfer(String fromAccountNum, String toAccountNum, long amount) {
 
+
+        long sendingAccMoney = accounts.get(fromAccountNum).getMoney();
+        long getterAccMoney = accounts.get(toAccountNum).getMoney();
+        long sendingAccMoneyAfterSend = sendingAccMoney - amount;
+        long getterAccMoneyAfterTransfet = getterAccMoney + amount;
+        accounts.get(fromAccountNum).setMoney(sendingAccMoneyAfterSend);
+        accounts.get(toAccountNum).setMoney(getterAccMoneyAfterTransfet);
+
     }
 
-    /**
-     * TODO: реализовать метод. Возвращает остаток на счёте.
-     */
-    public long getBalance(String accountNum) {
-        return 0;
-    }
 
     public long getSumAllAccounts() {
         return 0;
     }
 }
+
