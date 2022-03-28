4c7d83abdfa0e2a024dca5454b518b7b86344296
Александр Банкевич <aleksandr@bankevich.com>
2022-03-28 08:29:12 +0000

    Update Todo.java

--- SpringBootWebDevelopment/BookLibrary/TodoList/src/Main/java/Main/Todo.java
+++ SpringBootWebDevelopment/BookLibrary/TodoList/src/Main/java/Main/Todo.java
@@ -1,1 +1,17 @@
+package Main;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Todo {
+
+    private String name;
+    private int id;
+    public Todo(String name, int id){
+        this.id = id;
+        this.name = name;
+    }
+
+}
