931d5f5993e6aa54f7a76c7d066beefcacaba5d5
Александр Банкевич <aleksandr@bankevich.com>
2022-03-29 14:34:38 +0000

    Add new file

---
+++ SpringBootWebDevelopment/BookLibrary/TodoList/src/Main/java/Main/TodoRepository.java
@@ -0,0 +1,9 @@
+package Main.Model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends CrudRepository<Todo,Integer> {
+
+}
