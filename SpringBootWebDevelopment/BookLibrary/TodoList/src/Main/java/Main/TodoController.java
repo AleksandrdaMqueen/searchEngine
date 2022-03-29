9e88df6f5b9c0aaabfb0545b6203a1e72d4eca5d
Александр Банкевич <aleksandr@bankevich.com>
2022-03-29 14:34:15 +0000

    Update TodoController.java

--- SpringBootWebDevelopment/BookLibrary/TodoList/src/Main/java/Main/TodoController.java
+++ SpringBootWebDevelopment/BookLibrary/TodoList/src/Main/java/Main/TodoController.java
@@ -1,34 +1,67 @@
 package Main;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import Main.Model.Todo;
+import Main.Model.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TodoController {
+    @Autowired
+    private TodoRepository todoRepository;
+
 
     @RequestMapping(value = "/todos/",method = RequestMethod.GET)
     public List<Todo> list(){
-        return Storage.getTodos();
+        Iterable<Todo> todoIterable = todoRepository.findAll();
+        ArrayList<Todo> arrayList = new ArrayList<>();
+        for(Todo todo : todoIterable){
+            arrayList.add(todo);
+        }
+        return arrayList;
     }
 
+    
     @RequestMapping(value =  "/todos/",method = RequestMethod.POST)
-    public Todo add(String name){
-        return Storage.addTodo(name);
+    public int add(Todo todo){
+        Todo todo1 = todoRepository.save(todo);
+        return todo1.getId();
     }
+
 
     @GetMapping("/todos/{id}")
-    public Todo get(int id){
-        Todo todo =   Storage.getTodo(id);
-        return todo;
+    public ResponseEntity get(@PathVariable int id){
+        Optional<Todo> optionalTodo = todoRepository.findById(id);
+        if(!optionalTodo.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return new ResponseEntity(optionalTodo.get(), HttpStatus.OK);
     }
-    @RequestMapping(value = "todos", method = RequestMethod.DELETE)
+
+    @RequestMapping(value = "/todos/", method = RequestMethod.DELETE)
     public void deleteTodo(int id){
-        Storage.deleteTodo(id);
+        todoRepository.deleteById(id);
+    }
+
+
+    @RequestMapping(value = "/todos/", method = RequestMethod.DELETE)
+    public void deleteAllTodos(){
+        todoRepository.deleteAll();
+    }
+
+
+    @RequestMapping(value = "/todos/{id}",method = RequestMethod.PATCH)
+    public void editTodo(int id, String newName){
+        Optional<Todo> optionalTodo = todoRepository.findById(id);
+        Todo todoToEdit = optionalTodo.get();
+        todoToEdit.setName(newName);
+        todoRepository.save(todoToEdit);
     }
 
 }
