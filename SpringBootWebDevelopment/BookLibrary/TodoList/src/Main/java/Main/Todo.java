19baf2e2bb5a0ddeef42496b4bcd943330053750
Александр Банкевич <aleksandr@bankevich.com>
2022-03-29 14:36:23 +0000

    Update Todo.java

--- SpringBootWebDevelopment/BookLibrary/TodoList/src/Main/java/Main/Todo.java
+++ SpringBootWebDevelopment/BookLibrary/TodoList/src/Main/java/Main/Todo.java
@@ -1,13 +1,22 @@
-package Main;
+package Main.Model;
 
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Getter
 @Setter
 public class Todo {
 
     private String name;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     public Todo(String name, int id){
         this.id = id;
