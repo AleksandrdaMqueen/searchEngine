d197130016da03dda6e496e5dafa3a57a0ffe012
Александр Банкевич <aleksandr@bankevich.com>
2022-03-29 18:53:59 +0000

    Update DefaultController.java

--- SpringBootWebDevelopment/BookLibrary/TodoList/src/Main/java/Main/DefaultController.java
+++ SpringBootWebDevelopment/BookLibrary/TodoList/src/Main/java/Main/DefaultController.java
@@ -1,14 +1,34 @@
 package Main;
 
+
+import Main.Model.Todo;
+import Main.Model.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Date;
 
-@RestController
+@Controller
 public class DefaultCobtroller {
+
+    @Autowired
+    TodoRepository todoRepository;
+
     @RequestMapping("/")
-    public String index(){
-        return new Date().toString();
+    public String index(Model model){
+        Iterable<Todo> todoIterable = todoRepository.findAll();
+        ArrayList<Todo> todos = new ArrayList<>();
+        int todosCount = 0;
+        for(Todo todo : todoIterable){
+            todosCount++;
+            todos.add(todo);
+        }
+        model.addAttribute("todos", todos);
+        model.addAttribute("todosCount", todosCount);
+        return "index";
     }
 }
