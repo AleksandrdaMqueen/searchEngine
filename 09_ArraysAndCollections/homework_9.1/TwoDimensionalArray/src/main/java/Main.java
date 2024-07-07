edf5d8155e15fc7d43a0ef1ee54722e731d57f39
AleksandrdaMqueen <99744766+AleksandrdaMqueen@users.noreply.github.com>
2024-07-07 13:57:47 +0300

    Delete 09_ArraysAndCollections directory

--- 09_ArraysAndCollections/homework_9.1/TwoDimensionalArray/src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //Распечатайте сгенерированный в классе TwoDimensionalArray.java двумерный массив
-        System.out.println(TwoDimensionalArray.getTwoDimensionalArray(3));
-    }
-}
