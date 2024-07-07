5e74ccda2016395f5f7f80aefefbc7f6b29520e3
AleksandrdaMqueen <99744766+AleksandrdaMqueen@users.noreply.github.com>
2024-07-07 14:01:00 +0300

    Delete SpringBootWebDevelopment/BookLibrary directory

--- SpringBootWebDevelopment/BookLibrary/TodoList/src/Main/java/Main/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package Main.Model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoRepository extends CrudRepository<Todo,Integer> {
-
-}
